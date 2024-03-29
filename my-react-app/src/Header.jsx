5dd9a8e30ac749934c73a9671e811a8d78f49a9f
Jesemeil <otahaghogho@gmail.com>
2024-03-29 02:49:28 +0100

    Practice

---
+++ my-react-app/src/Header.jsx
@@ -0,0 +1,18 @@
+function Header() {
+  return (
+    <header>
+      <h1>My Website</h1>
+      <nav>
+        <ul>
+          <li>Home</li>
+          <li>Services</li>
+          <li>Contact</li>
+          <li>FAQs</li>
+        </ul>
+        <hr></hr>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
