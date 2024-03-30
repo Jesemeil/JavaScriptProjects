8a7350b360faad8604be4d8e4eb6a8b540c1518d
Jesemeil <otahaghogho@gmail.com>
2024-03-30 13:03:57 +0100

    Added UserGreeting

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -3,6 +3,7 @@ import Footer from "./Footer";
 import Card from "./Card";
 import Button from "./Button/Button";
 import Student from "./Student";
+import UserGreeting from "./UserGreeting";
 
 function App() {
   return (
@@ -16,6 +17,8 @@ function App() {
       <Student name="Aghogho" age={27} isStudent={true} />
       <Student name="Akpan" age="78" isStudent={false} />
       <Student />
+      <UserGreeting isLoggedIn={true} username="AJ Code" />
+      <UserGreeting />
     </>
   );
 }
