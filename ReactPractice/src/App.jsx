ecad39b2886dda62a2264d6e1dbb341df38c98a2
Jesemeil <otahaghogho@gmail.com>
2024-04-01 23:52:53 +0100

    Added MyComponent

---
+++ ReactPractice/src/App.jsx
@@ -0,0 +1,13 @@
+import MyComponent from "./MyComponent";
+import Welcome from "./Welcome";
+
+function App() {
+  return (
+    <>
+      <Welcome />
+      <MyComponent />
+    </>
+  );
+}
+
+export default App;
---
+++ ReactPractice/src/MyComponent.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from "react";
+function MyComponent() {
+  const [name, setName] = useState("Guest");
+
+  const handleNameChange = (e) => {
+    setName(e.target.value);
+  };
+  return (
+    <div>
+      <input value={name} onChange={handleNameChange} />
+      <p>Name:{name} </p>
+    </div>
+  );
+}
+export default MyComponent;
