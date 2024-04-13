9dff1b95e6a9a1b833e2d5b3975978d4c2022650
Jesemeil <otahaghogho@gmail.com>
2024-04-13 19:24:27 +0100

    Modified ComponentA

--- useEffectPractice/src/ComponentA.jsx
+++ useEffectPractice/src/ComponentA.jsx
@@ -1,7 +1,14 @@
+import React, { useState } from "react";
+import ComponentB from "./ComponentB";
+
 function ComponentA() {
+  const [user, setUser] = useState("AJCode");
+
   return (
     <div className="box">
       <h1>ComponentA</h1>
+      <h2>{`Hello ${user}`}</h2>
+      <ComponentB user={user} />
     </div>
   );
 }
