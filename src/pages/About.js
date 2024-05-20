eae2d8d62bd2d1a447d97d0295fcdb6a46bd1bbd
Jesemeil <otahaghogho@gmail.com>
2024-05-20 15:36:44 +0100

    first commit

---
+++ src/pages/About.js
@@ -0,0 +1,18 @@
+export default function About() {
+  return (
+    <div className="about">
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Recusandae
+        asperiores unde laudantium dolores, quibusdam cupiditate harum omnis
+        facilis necessitatibus quasi eligendi numquam ipsum possimus distinctio
+        suscipit repellendus laborum aliquam ducimus!
+      </p>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Animi
+        excepturi mollitia nemo. Culpa distinctio qui, minima omnis accusantium
+        quae impedit rerum quibusdam harum consequatur. Obcaecati natus autem
+        repellendus ut quis.
+      </p>
+    </div>
+  );
+}
---
+++ src/pages/Home.js
@@ -0,0 +1,17 @@
+export default function Home() {
+  return (
+    <div className="home">
+      <h1>Welcome</h1>
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officia
+      voluptate atque culpa perferendis sed nostrum ab aperiam ex laudantium
+      autem animi rerum architecto, similique numquam praesentium amet nesciunt
+      quae voluptatum. Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+      Quod perspiciatis neque doloribus dolorum quis reiciendis vero voluptates
+      incidunt, eligendi aliquid fuga delectus dolores soluta totam iusto ad
+      omnis quos aspernatur! Lorem ipsum dolor sit amet consectetur adipisicing
+      elit. Tenetur modi adipisci, quo delectus, quidem, aperiam nisi vel dicta
+      expedita dolorem nobis! Ullam earum beatae saepe sequi assumenda aliquam
+      sunt cum!
+    </div>
+  );
+}
